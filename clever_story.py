69076144220354e1349bc4c8c420b34bbe6033b1
ievbosaru <ievbosaru@byupathway.edu>
2025-03-12 08:38:56 -0700

    10

---
+++ clever_story.py
@@ -0,0 +1,23 @@
+# clever story
+# this program prompts the user to enter a number and then prints out a clever story
+print("welcome to the clever story program")
+print("answer the following questions to create a clever story")
+
+#prompt the user for input and saving to variables
+name = input("samuel: ")
+adjective = input("good: ")
+noun = input("dog: ")
+verb = input("run: ")
+adjective2 = input("happy: ")
+noun2 = input("cat: ")
+exclamation = input("wow: ")
+
+#exclamation = exclamation.capitalize()
+
+#output the story
+print("Once upon a time, there was a " + name + " who was very " + adjective + ".")
+print("He had a " + noun + " that loved to " + verb + ".")
+print("The " + noun + " was very " + adjective2 + " and loved to play with a " + noun2 + ".")
+print("One day, the " + noun + " said, '" + exclamation + "!'")
+print("The " + name + " was very surprised and said, 'Wow, that was clever!'")
+print("The " + noun + " was very " + adjective + " and " + verb + " away.")
